a2aa4f72490b66742bb1a67b5945efd9e2cdae6e
NadiyaShrai <134630278+deagorn@users.noreply.github.com>
2023-08-09 21:05:02 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientsArr = [];
+
+ingredients.forEach(ingredient => {
+const ingredientEl = document.createElement("li");
+ingredientEl.textContent = ingredient;
+ingredientEl.classList = "item";
+
+ingredientsArr.push(ingredientEl);
+console.log(ingredientsArr);
+});
+
+const list = document.querySelector('#ingredients');
+list.after(...ingredientsArr);
