4174af9a1929f8bae5a866bfa035c658b6c868ac
NadiyaShrai <134630278+deagorn@users.noreply.github.com>
2023-08-09 21:38:58 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,10 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const markup = images
+  .map((image) => `<li> <img src=${image.url} alt=${image.alt} /></li>`)
+  .join("");
+ 
+const list = document.querySelector(".gallery");
+list.insertAdjacentHTML("afterbegin",markup) ;
