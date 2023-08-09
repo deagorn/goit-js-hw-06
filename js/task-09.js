195dbe66b71acd70619df47968a472295837ab7c
NadiyaShrai <134630278+deagorn@users.noreply.github.com>
2023-08-10 02:04:16 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnChangeColor = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const nameColor = document.querySelector(".color");
+
+btnChangeColor.addEventListener("click", onChangeColor);
+
+function onChangeColor() {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = `${color}`; 
+  nameColor.textContent = color;
+}
