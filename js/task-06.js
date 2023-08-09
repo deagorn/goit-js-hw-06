1cfab8254cc48fdb155e2d789bee5ed1ee86be5f
NadiyaShrai <134630278+deagorn@users.noreply.github.com>
2023-08-09 23:05:12 +0300

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,18 @@
+const validationInput = document.querySelector("#validation-input");
+
+validationInput.addEventListener("blur", onInputValidation);
+
+const dataLengthValue = validationInput.dataset.length;
+
+
+function onInputValidation(event) {
+ 
+    if (event.currentTarget.value.length==dataLengthValue) {
+        validationInput.classList.remove("invalid");
+        validationInput.classList.add("valid");
+    }
+    else {
+        validationInput.classList.add("invalid");
+        validationInput.classList.remove("valid");
+    }
+}
