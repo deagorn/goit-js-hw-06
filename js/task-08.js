900bbe999324d6687a010ea6af5255149fc2a3e6
NadiyaShrai <134630278+deagorn@users.noreply.github.com>
2023-08-10 21:14:14 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,20 +1,17 @@
 const btnSubmit = document.querySelector(".login-form");
-const formData = {};
 
 btnSubmit.addEventListener("submit", onSubmit);
 
 function onSubmit(event) {
     event.preventDefault();
     
-//   const {
-//     elements: { username, password }
-//   } = event.currentTarget.elements;
     
-    const arr = event.currentTarget.elements;
+    const { email, password } = event.currentTarget.elements;
 
-    for (let i = 0; i < arr.length-1; i++){
-        formData[arr[i].name] = arr[i].value;
-    }
+    const formData = {
+        email: email.value,
+        password: password.value
+    };
  
     if (formData.email === '' || formData.password === '') {
         alert('All fields must be filled')
